59bed438cbbb771becca24fbe7146336be4312b3
303 <303@academicos.cesde.edu.co>
2022-11-19 16:52:28 -0500

    se crea

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HabitacionesService } from '../services/habitaciones.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent  {
+
+  public habitaciones:any[]=[]
+
+  constructor(public servicioHabitacion:HabitacionesService) { 
+
+    this.servicioHabitacion.consultarHabitaciones()
+    .subscribe(respuesta=>{
+      this.habitaciones=respuesta
+    })
+
+  }
+
+  
+
+}
